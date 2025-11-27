03dd22f00228951db9d295a72cbc59ee2265d77b
Scott Hoge <122124436+MonkeyBizScott@users.noreply.github.com>
2025-11-27 13:20:35 -0800

    Initial Commit

---
+++ LemonSliceDemo/LemonSliceDemoApp.swift
@@ -0,0 +1,16 @@
+//
+//  LemonSliceDemoApp.swift
+//
+//  Created by Scott on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonSliceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LemonSliceDemoTests/LemonSliceDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  LemonSliceDemoTests.swift
+//
+//  Created by Scott on 11/27/25.
+//
+
+import Testing
+@testable import LemonSliceDemo
+
+struct LemonSliceDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
